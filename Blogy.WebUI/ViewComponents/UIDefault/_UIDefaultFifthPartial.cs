ccb7c6f6d26cc77ce5527c4192f3eb7b80624cd6
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-16 21:28:10 +0300

    Default view components have been created

---
+++ Blogy.WebUI/ViewComponents/UIDefault/_UIDefaultFifthPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents.UIDefault;
+
+public class _UIDefaultFifthPartial : ViewComponent
+{
+	public IViewComponentResult Invoke()
+	{
+		return View();
+	}
+}
---
+++ Blogy.WebUI/ViewComponents/UIDefault/_UIDefaultFourthPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents.UIDefault;
+
+public class _UIDefaultFourthPartial : ViewComponent
+{
+	public IViewComponentResult Invoke()
+	{
+		return View();	
+	}
+}
