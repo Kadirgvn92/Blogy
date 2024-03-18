3c6c5e2b7c8d9d9a6a2d4808a485d4111374b9b9
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-18 10:56:15 +0300

    Admin dashboard completed

---
+++ Blogy.WebUI/Areas/Admin/ViewComponents/_AdminDashboardRecentlyPartial.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardRecentlyPartial : ViewComponent
+{
+    private readonly IArticleService _articleService;
+
+    public _AdminDashboardRecentlyPartial(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _articleService.TGetAllArticles();
+        return View(values);  
+    }
+}
---
+++ Blogy.WebUI/Areas/Admin/ViewComponents/_AdminDashboardWriterPartial.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardWriterPartial : ViewComponent
+{
+    private readonly IWriterService _writerService;
+
+    public _AdminDashboardWriterPartial(IWriterService writerService)
+    {
+        _writerService = writerService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _writerService.TGetAll();
+        return View(values);  
+    }
+}
