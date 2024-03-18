0fb1eec5f8285d6db503d754ded02cf55584e389
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-18 10:06:33 +0300

    Dashboard started to develop

--- Blogy.WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogy.WebUI.Areas.Admin.Controllers;
 
@@ -6,8 +7,28 @@ namespace Blogy.WebUI.Areas.Admin.Controllers;
 [Route("Admin/[controller]/[action]/{id?}")]
 public class DashboardController : Controller
 {
+    private readonly IArticleService _articleService;
+    private readonly ICategoryService _categoryService;
+    private readonly ICommentService _commentService;
+
+    public DashboardController(IArticleService articleService, ICategoryService categoryService, ICommentService commentService)
+    {
+        _articleService = articleService;
+        _categoryService = categoryService;
+        _commentService = commentService;
+    }
+
     public IActionResult Index()
     {
+        var article1 = _articleService.TGetAll().Count();
+        ViewBag.Article1 = article1;    
+
+        var article2 = _categoryService.TGetAll().Count();
+        ViewBag.Article2 = article2;
+
+        var comment = _commentService.TGetAll().Count();
+        ViewBag.Comment1 = comment;    
+
         return View();
     }
 }
