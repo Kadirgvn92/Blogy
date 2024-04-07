a460514e0c298a2f93b7b8b47a2f703d716fcabe
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-07 14:17:35 +0300

    Searching mechanism completed

---
+++ Blogy.WebUI/Areas/Admin/Models/SearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Blogy.EntityLayer;
+
+namespace Blogy.WebUI.Areas.Admin.Models;
+
+public class SearchViewModel
+{
+    public int ArticleID { get; set; }
+    public string Title { get; set; }
+    public string FirstSection { get; set; }
+    public string SecondSection { get; set; }
+    public string? ThirdSection { get; set; }
+    public string? FourthSection { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string Description { get; set; }
+    public string? CoverImageUrl { get; set; }
+    public int WriterID { get; set; }
+    public Writer Writer { get; set; }
+    public int CategoryID { get; set; }
+    public Category Categories { get; set; }
+    public List<Comment> Comments { get; set; }
+    public int TotalArticles { get; set; }
+    public PageInfoModel PageInfo { get; set; }
+    public List<Article> Articles { get; set; }
+    public AppUser AppUser { get; set; }
+    public string search {  get; set; }
+}
---
+++ Blogy.WebUI/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Blogy.WebUI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Drawing.Printing;
+
+namespace Blogy.WebUI.Controllers;
+public class SearchController : Controller
+{
+    private readonly IArticleService _articleService;
+
+    public SearchController(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    public IActionResult Index(string searchString,int page = 1)
+    {
+        ViewData["CurrentFilter"] = searchString;
+        ViewBag.SearchString = searchString;
+
+        var valueSeach = from x in _articleService.TGetAllArticles() select x;
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            searchString = searchString.ToLower();
+            valueSeach = valueSeach.Where(y => y.Title.ToLower().Contains(searchString));
+        }
+
+        const int pageSize = 3;
+        var model = new SearchViewModel
+        {
+            PageInfo = new PageInfoModel()
+            {
+                TotalItems = valueSeach.Count(),
+                CurrentPage = page,
+                ItemsPerPage = pageSize,
+            },
+            Articles = valueSeach.OrderByDescending(x => x.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize).ToList(),
+            TotalArticles = valueSeach.Count(),
+            search = searchString
+        };
+        return View(model);
+    }
+}
