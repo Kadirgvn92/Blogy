c3b6908bbe4ea1dc691dd50de76ce8980822135b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-05-01 17:42:04 +0300

    I tried to js datatable but not worked. I completed Commentlist with AjAX

---
+++ Blogy.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,47 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class CommentController : Controller
+{
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+    public IActionResult Index()
+    {
+        var comments = _commentService.TGetAll();
+        return View(comments);
+    }
+    [HttpGet]
+    public IActionResult GetComment()
+    {
+        // Comment nesnelerini al
+        var comments = _commentService.TGetAll();
+
+        // Comment nesnelerini JSON formatına dönüştür
+        var jsonComments = JsonConvert.SerializeObject(comments);
+
+        // JSON formatındaki Comment nesnelerini View'e gönder
+        return Content(jsonComments, "application/json");
+    }
+    public IActionResult AcceptedCommentList()
+    {
+        
+        return View();
+    }
+    public IActionResult WaitingCommentList()
+    {
+        return View();
+    }
+    public IActionResult CanceledCommentList()
+    {
+        return View();
+    }
+}
