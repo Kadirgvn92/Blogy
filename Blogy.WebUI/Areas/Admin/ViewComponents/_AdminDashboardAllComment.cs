28d4f3a0ebcf87eb69a7ff1721e1ffe30927671e
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-05-04 16:36:48 +0300

    Dashboard comment completed

---
+++ Blogy.WebUI/Areas/Admin/ViewComponents/_AdminDashboardAllComment.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardAllComment : ViewComponent
+{
+    private readonly ICommentService _commentService;
+
+    public _AdminDashboardAllComment(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _commentService.TGetAll();
+        return View(values);
+    }
+}
---
+++ Blogy.WebUI/Areas/Admin/ViewComponents/_AdminDashboardWaitingCommentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardWaitingCommentPartial : ViewComponent
+{
+    private readonly ICommentService _commentService;
+
+    public _AdminDashboardWaitingCommentPartial(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _commentService.TGetWaitingComments();
+        return View(values);
+    }
+}
