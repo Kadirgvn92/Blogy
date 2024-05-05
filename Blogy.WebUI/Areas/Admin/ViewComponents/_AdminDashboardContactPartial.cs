b2148fc0803e49cfb1fec24a9ef1a032a2549984
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-05-05 21:27:35 +0300

    Contact page started

---
+++ Blogy.WebUI/Areas/Admin/ViewComponents/_AdminDashboardContactPartial.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardContactPartial : ViewComponent
+{
+    private readonly IContactService _contactService;
+
+    public _AdminDashboardContactPartial(IContactService contactService)
+    {
+        _contactService = contactService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var values = _contactService.TGetAll();
+        return View(values);
+    }
+}
