ebfa783ba75c6946e0df7c7e39bdd5e997aae240
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-05-12 17:53:01 +0300

    Member Dashboard rearranged

--- Blogy.WebUI/ViewComponents/MemberLayout/_MemberLayoutSidebarPartial.cs
+++ Blogy.WebUI/ViewComponents/MemberLayout/_MemberLayoutSidebarPartial.cs
@@ -9,17 +9,21 @@ public class _MemberLayoutSidebarPartial : ViewComponent
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly IWriterService _writerService;
+    private readonly IArticleService _articleService;
 
-    public _MemberLayoutSidebarPartial(UserManager<AppUser> userManager, IWriterService writerService)
+    public _MemberLayoutSidebarPartial(UserManager<AppUser> userManager, IWriterService writerService, IArticleService articleService)
     {
         _userManager = userManager;
         _writerService = writerService;
+        _articleService = articleService;
     }
 
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        var user = _userManager.FindByNameAsync(User.Identity.Name);
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
         var writer = _writerService.TGetWriter(user.Id);
+        var articles = _articleService.TGetAllArticles().Where(x => x.WriterID == writer.WriterID).Count();
+        ViewBag.Articles = articles;
         ViewBag.Writer = writer.Name;
         return View();
     }
